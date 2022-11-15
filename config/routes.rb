04f73a1f4d1609d9fa35b57dac22d13e02011803
Max-Wuschko <mw10.wuschko@freenet.de>
2022-11-15 17:01:27 +0100

    challenge 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   resources :tasks
-  get "tasks",             to: "tasks#new", as: :tasks_new
-  delete "tasks/:id",      to: "tasks#destroy", as: :delete_task
 end
